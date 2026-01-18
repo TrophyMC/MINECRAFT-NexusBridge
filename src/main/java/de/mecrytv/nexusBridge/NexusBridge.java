c763b68eaa8c05a9cacda0dc90817a3415e509b8
MecryTv <Development@mecrytv.de>
2026-01-18 16:20:55 +0100

    [EDIT] Getter for dataDirectory

--- src/main/java/de/mecrytv/nexusBridge/NexusBridge.java
+++ src/main/java/de/mecrytv/nexusBridge/NexusBridge.java
@@ -80,4 +80,7 @@ public class NexusBridge {
     public DatabaseAPI getDatabaseAPI() {
         return databaseAPI;
     }
+    public Path getDataDirectory() {
+        return dataDirectory;
+    }
 }
